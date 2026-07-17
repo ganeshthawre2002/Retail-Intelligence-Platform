bf135a43a61c04c1aaaf8ed648f2fad31f27491a
Ganesh thawre <Ganeshthawre5@gamil.com>
2026-07-18 01:36:57 +0530

    feat: create category_translation table

---
+++ sql/ddl/create_schema.sql
@@ -0,0 +1,16 @@
+-- =====
+-- Retail Intelligence Platform 
+-- Create Database Schema
+-- =====
+
+CREATE SCHEMA IF NOT EXISTS retail;
+
+COMMENT ON SCHEMA retail IS
+'Operational schema for the Retail Intelligence Platform.';
+
+
+
+
+SELECT current_user;
+
+SHOW port;
